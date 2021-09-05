77a6ef1cba9be3e973158463dfc25bf35a500648
Dmitrii Okunev <xaionaro@dx.center>
2021-09-05 22:47:06 +0100

    Copy all files in parallel.

--- file_tree.go
+++ file_tree.go
@@ -10,19 +10,25 @@ import (
 	"path"
 	"path/filepath"
 	"sort"
+	"sync"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/xaionaro-go/errors"
 )
 
 type node struct {
+	path string
 	size int64
 }
 
 type fileTree struct {
-	nodes map[string]node
-
 	rootPath string
+
+	nodeChan     chan node
+	nodeMap      map[string]node
+	nodeMapMutex sync.Mutex
+
+	scanWg sync.WaitGroup
 
 	cachePath string
 	cacheDB   *sql.DB
@@ -30,6 +36,7 @@ type fileTree struct {
 	brokenFilesList     *os.File
 	brokenFilesListPath string
 	brokenFilesMap      map[string]bool
+	brokenFilesMapMutex sync.Mutex
 }
 
 type FileTree interface {
@@ -44,9 +51,12 @@ func GetFileTree(dir string) (FileTree, error) {
 		return nil, err
 	}
 	ft := &fileTree{
-		rootPath: dir,
+		rootPath:       dir,
+		nodeChan:       make(chan node, 1024),
+		nodeMap:        map[string]node{},
+		brokenFilesMap: map[string]bool{},
 	}
-	err = ft.Scan()
+	err = ft.scan()
 	if err != nil {
 		return nil, errors.New(err)
 	}
@@ -60,8 +70,11 @@ func GetCachedFileTree(dir, cachePath string) (FileTree, error) {
 		return nil, err
 	}
 	ft := &fileTree{
-		rootPath:  dir,
-		cachePath: cachePath,
+		rootPath:       dir,
+		cachePath:      cachePath,
+		nodeChan:       make(chan node, 1024),
+		nodeMap:        map[string]node{},
+		brokenFilesMap: map[string]bool{},
 	}
 
 	hasCache := true
@@ -76,9 +89,12 @@ func GetCachedFileTree(dir, cachePath string) (FileTree, error) {
 	ft.cacheDB.SetMaxOpenConns(1)
 
 	if hasCache {
-		log.Println("Reading the cache from", ft.cachePath)
-		err = ft.ReadCache()
-		log.Println("Reading the cache from", ft.cachePath, "-- complete")
+		go func() {
+			log.Println("Reading the cache from", ft.cachePath)
+			err = ft.readCache()
+			log.Println("Reading the cache from", ft.cachePath, "-- complete")
+			close(ft.nodeChan)
+		}()
 	} else {
 		_, err = ft.cacheDB.Exec(`CREATE TABLE file_tree (path varchar(4096), size bigint)`)
 		if err != nil {
@@ -88,7 +104,14 @@ func GetCachedFileTree(dir, cachePath string) (FileTree, error) {
 		if err != nil {
 			return nil, errors.New(err)
 		}
-		err = ft.Scan()
+		err := ft.scan()
+		if err != nil {
+			return nil, errors.New(err)
+		}
+		go func() {
+			ft.scanWg.Wait()
+			close(ft.nodeChan)
+		}()
 	}
 	if err != nil {
 		return nil, errors.New(err)
@@ -97,24 +120,20 @@ func GetCachedFileTree(dir, cachePath string) (FileTree, error) {
 	return ft, nil
 }
 
-func (ft *fileTree) ReadCache() error {
+func (ft *fileTree) readCache() error {
 	rows, err := ft.cacheDB.Query("SELECT path, size FROM file_tree")
 	if err != nil {
 		return errors.New(err)
 	}
 
-	ft.nodes = map[string]node{}
-
 	defer rows.Close()
 
 	for rows.Next() {
-		var filePath string
-		var fileSize int64
-		rows.Scan(&filePath, &fileSize)
+		var node node
+		rows.Scan(&node.path, &node.size)
 
-		ft.nodes[filePath] = node{
-			size: fileSize,
-		}
+		ft.nodeChan <- node
+		ft.nodeMap[node.path] = node
 	}
 
 	err = rows.Err()
@@ -125,93 +144,121 @@ func (ft *fileTree) ReadCache() error {
 }
 
 func (ft *fileTree) addNode(path string, node node) {
-	ft.nodes[path] = node
+	ft.nodeMapMutex.Lock()
+	ft.nodeMap[path] = node
+	ft.nodeMapMutex.Unlock()
+	ft.nodeChan <- node
 	if ft.cacheDB != nil {
 		ft.cacheDB.Exec(`INSERT INTO file_tree (path, size) VALUES (?, ?)`, path, node.size)
 	}
 }
 
-func (ft *fileTree) Scan() error {
+func (ft *fileTree) scan() error {
 	log.Println("Scanning", ft.rootPath)
 	defer log.Println("Scanning", ft.rootPath, "-- complete")
+
+	return ft.scanDir(ft.rootPath)
+}
+
+func (ft *fileTree) scanDir(rootPath string) error {
+	ft.scanWg.Add(1)
+	defer ft.scanWg.Done()
 
 	if ft.cacheDB != nil {
 		ft.cacheDB.Exec(`START TRANSACTION`)
 		defer ft.cacheDB.Exec(`COMMIT`)
 	}
+	f, err := os.Open(rootPath)
+	if err != nil {
+		return errors.New(err)
+	}
+	defer f.Close()
+	for {
+		list, err := f.Readdir(1)
+		if err != nil {
+			if err != io.EOF {
+				ft.addBrokenFile(rootPath, err)
+			}
+			break
+		}
 
-	ft.nodes = map[string]node{}
-	return filepath.Walk(ft.rootPath, func(filePath string, info os.FileInfo, err error) error {
-		if err != nil {
-			err = ft.addBrokenFile(filePath, err)
-			return nil
+		for _, fileInfo := range list {
+			if fileInfo.IsDir() {
+				ft.scanWg.Add(1)
+				go func(fileInfo os.FileInfo) {
+					defer ft.scanWg.Done()
+					filePath := filepath.Join(rootPath, fileInfo.Name())
+					err := ft.scanDir(filePath)
+					if err != nil {
+						ft.addBrokenFile(filePath, err)
+					}
+				}(fileInfo)
+				continue
+			}
+
+			pathRel, err := filepath.Rel(ft.rootPath, filepath.Join(rootPath, fileInfo.Name()))
+			if err != nil {
+				return errors.New(err)
+			}
+			ft.addNode(pathRel, node{size: fileInfo.Size()})
 		}
-		if info.IsDir() {
-			return nil
-		}
-		pathRel, err := filepath.Rel(ft.rootPath, filePath)
-		if err != nil {
-			return errors.New(err)
-		}
-		ft.addNode(pathRel, node{size: info.Size()})
-		return nil
-	})
+	}
+
+	return nil
 }
 func (src *fileTree) SyncTo(dstI FileTree, dryRun bool) error {
 	log.Println("Syncing")
 	defer log.Println("Syncing -- complete")
 
-	filesToCopy := []string{}
+	var filesToCopy []string
 
 	dst := dstI.(*fileTree)
+	for range dst.nodeChan {
+	}
 
-	for filePath, srcNode := range src.nodes {
+	for srcNode := range src.nodeChan {
 		if src.brokenFilesMap != nil {
-			if src.brokenFilesMap[filePath] {
+			if src.brokenFilesMap[srcNode.path] {
 				continue
 			}
 		}
 
-		dstNode := dst.nodes[filePath]
+		dstNode := dst.nodeMap[srcNode.path]
 		if srcNode.size == dstNode.size {
 			continue
 		}
-		filesToCopy = append(filesToCopy, filePath)
+		filesToCopy = append(filesToCopy, srcNode.path)
 	}
 
 	sort.Strings(filesToCopy)
 
-	/*fmt.Println("to copy")
+	fmt.Println("to copy")
 	for _, filePath := range filesToCopy {
 		fmt.Println(filePath)
 	}
-	fmt.Println("to copy -- complete")*/
+	fmt.Println("to copy -- complete")
 
-	onePercentCount := (len(filesToCopy) + 99) / 100
-
-	for idx, filePath := range filesToCopy {
-		/*if idx%onePercentCount == 0 {
-			fmt.Println(idx/onePercentCount, "%")
-		}*/
-		fmt.Println(idx/onePercentCount, "%: coping:", filePath)
+	for _, filePath := range filesToCopy {
 		if dryRun {
 			continue
 		}
 
-		dstDir := filepath.Dir(path.Join(dst.rootPath, filePath))
-		err := createDirectory(dstDir)
-		if err != nil {
-			fmt.Println("cannot create directory", dstDir)
-			continue
-		}
-		err = copyFileContents(path.Join(src.rootPath, filePath), path.Join(dst.rootPath, filePath))
-		if err != nil {
-			// TODO: consider possible errors on the destination side
-			err = src.addBrokenFile(filePath, err)
+		go func(filePath string) {
+			dstDir := filepath.Dir(path.Join(dst.rootPath, filePath))
+			err := createDirectory(dstDir)
 			if err != nil {
-				return errors.New(err)
+				fmt.Println("cannot create directory", dstDir)
+				return
 			}
-		}
+			err = copyFileContents(path.Join(src.rootPath, filePath), path.Join(dst.rootPath, filePath))
+			if err != nil {
+				// TODO: consider possible errors on the destination side
+				err = src.addBrokenFile(filePath, err)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}(filePath)
 	}
 
 	return nil
@@ -236,8 +283,6 @@ func (ft *fileTree) SetBrokenFilesList(path string) error {
 		return errors.New(err, ft.brokenFilesListPath)
 	}
 
-	ft.brokenFilesMap = map[string]bool{}
-
 	scanner := bufio.NewScanner(ft.brokenFilesList)
 	for scanner.Scan() {
 		ft.brokenFilesMap[scanner.Text()] = true
@@ -255,8 +300,10 @@ func (src *fileTree) addBrokenFile(filePath string, fileErr error) error {
 	if src.brokenFilesList == nil {
 		return nil
 	}
+	src.brokenFilesMapMutex.Lock()
+	defer src.brokenFilesMapMutex.Unlock()
 	src.brokenFilesMap[filePath] = true
-	_, err := src.brokenFilesList.Write([]byte(fmt.Sprintf("%s\n", src)))
+	_, err := src.brokenFilesList.Write([]byte(fmt.Sprintf("%s\n", filePath)))
 	return errors.Wrap(err)
 }
 
@@ -285,7 +332,8 @@ func copyFileContents(src, dst string) (err error) {
 
 	buf := make([]byte, 1024*1024)
 
-	writeResultChan := make(chan error)
+	writeResultChan := make(chan error, 1)
+	writeResultChan <- nil
 
 	for {
 		rn, err := io.ReadFull(in, buf)
@@ -314,8 +362,8 @@ func copyFileContents(src, dst string) (err error) {
 		}(rn)
 	}
 
-	if _, err = io.Copy(out, in); err != nil {
+	/*if _, err = io.Copy(out, in); err != nil {
 		return errors.New(err)
-	}
+	}*/
 	return
 }
